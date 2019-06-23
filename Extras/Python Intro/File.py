116cb4b0a57ce807c501f1f5e995b6baa8b19915
Lersgeeb <banegas.gabriel13@gmail.com>
2019-06-23 14:13:06 -0600

    Modificacion_codigo_de_File

--- Extras/Python Intro/File.py
+++ Extras/Python Intro/File.py
@@ -1,4 +1,4 @@
-myFile = open("MyTxtFile")  #Inicializar el archivo de texto
+myFile = open("MyTxtFile.txt","r")  #Inicializar el archivo de texto
 miStr = myFile.read()   #retorna todas las lineas en forma de string
 print("miStr")
                         #de usarse una segunda vez no retornaria nada almenso que:
@@ -9,12 +9,12 @@ print(miStr)
 
 myFile.close()  #Cierra el archivo de texto
 
-with open("MyTxtFile") as myNewFile: #Otra forma de abrir un archivo de texto que lo cierra
+with open("MyTxtFile.txt") as myNewFile: #Otra forma de abrir un archivo de texto que lo cierra
                                      #automaticamente una vez fuera del bloque
     content = myNewFile.read()
     print(content)
 
-with open("MyTxtFile",mode = "a") as myNewFile2: #w: escribir sobre un archivo
+with open("MyTxtFile.txt",mode = "a") as myNewFile2: #w: escribir sobre un archivo
                                         #r: leer el archivo
                                         #a:añadir mas lineas a un archivo
                                         #r+:leer y escribir sobre un archivo
@@ -22,6 +22,6 @@ with open("MyTxtFile",mode = "a") as myNewFile2: #w: escribir sobre un archivo
     myNewFile2.write("\nmi cuarta linea")
     print(myNewFile2)
 
-with open("MyTxtFile","w") as myNewFile3:
+with open("MyTxtFile.txt","w") as myNewFile3:
     myNewFile3.write("\nmi quinta linea")    
 
