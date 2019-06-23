6f3ad4ba283b8357981ede4e0c8b7140715e0bc6
Lersgeeb <banegas.gabriel13@gmail.com>
2019-06-23 15:27:26 -0600

    Turtle y Sets Cambios

--- Extras/Python Intro/Sets.py
+++ Extras/Python Intro/Sets.py
@@ -2,6 +2,8 @@ mySet = set()
 mySet.add(1)
 mySet.add(2)
 mySet.add(2) #no se agregara un valor que ya existe 
+print(mySet)
 
-myList[1,1,1,2,2,2,3,3,3,3]
+myList = [1,1,1,2,2,2,3,3,3,3]
 myNewSet=set(myList)#solo se agregara un valor de cada valor repetido
+print(myNewSet)
