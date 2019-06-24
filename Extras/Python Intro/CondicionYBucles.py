2267cdc8b645578b4538935bc0e591ea450a483d
Lersgeeb <banegas.gabrie13@gmail.com>
2019-06-23 19:52:19 -0600

    Algunas cosas de Bucles

--- Extras/Python Intro/CondicionYBucles.py
+++ Extras/Python Intro/CondicionYBucles.py
@@ -19,3 +19,22 @@ while(keep < 3):
     
     print("keep es" , str(keep))
     keep +=1       
+
+myList =[(1,2),(3,4),(5,6),(7,8)]
+for (a,b) in myList:
+    print(a)
+    print(b)
+
+miDic ={"k1":1,"k2":2,"k3":3}
+for item in miDic:
+    print(item) #solo se imprimira la declaracion(key)
+
+for value in miDic.values():
+    print(item) #solo se imprimira el valor(value)
+
+for d in miDic.items():
+    print(d) #imprimira la declaracion(key) y el valor(value)
+
+for (item,value) in miDic.items():
+    print(item) #imprimira la declaracion(key) 
+    print(value) #imprimira el valor(value)
