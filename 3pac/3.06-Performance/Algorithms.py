9c6474e971fca3f530cce6f7905083f174da583e
Lersgeeb <bangeas.gabriel13@gmail.com>
2019-10-22 09:39:52 -0600

    Agregando MergeSort

--- 3pac/3.06-Performance/Algorithms.py
+++ 3pac/3.06-Performance/Algorithms.py
@@ -85,6 +85,28 @@ class Quicksort:
         self.quickSort(data,0,len(data)-1)
         return data
 
+class MergeSort:
 
+    def sort(self,values= []):
+    
+        if len(values) <= 1:
+            return values
 
+        middle_index = len(values) // 2
+        left_values = self.sort(values[:middle_index])
+        right_values = self.sort(values[middle_index:])
+        sorted_values = []
+        left_index = 0
+        right_index = 0
+        while left_index < len(left_values) and right_index < len(right_values):
+            if left_values[left_index] < right_values[right_index]:
+                sorted_values.append(left_values[left_index])
+                left_index += 1
+            else:
+                sorted_values.append(right_values[right_index])
+                right_index += 1
+        sorted_values += left_values[left_index:]
+        sorted_values += right_values[right_index:]
+        
+        return sorted_values
 
--- 3pac/3.06-Performance/main.py
+++ 3pac/3.06-Performance/main.py
@@ -6,12 +6,13 @@ from Performance import *
 execution = ExecutionTime()
 test = TestPerformance()
 arrayGenerator = ArrayGenerator()
-array = arrayGenerator.generate(1000)
+array = arrayGenerator.generate(10000)
 
 bubbleSort = BubbleSort()
 insertionSort = InsertionSort()
 selectionSort = SelectionSort()
 quickSort = Quicksort()
+mergeSort = MergeSort()
 
 bubble = test.test(bubbleSort,execution, array[:])
 
@@ -21,8 +22,11 @@ selection = test.test(selectionSort, execution, array[:])
 
 quick = test.test(quickSort, execution, array[:])
 
-print("Algoritmo  #Datos  Tiempo(ms)")
-print(bubble)
-print(insertion)
-print(selection)
-print(quick)
+merge = test.test(mergeSort, execution, array[:])
+
+print("Algoritmo\t\t\tTiempo(ms)")
+print("BubbleSort time: \t\t%s" % bubble[1])
+print("InsertionSort time: \t\t%s" % insertion[1])
+print("SelectionSort time: \t\t%s" % selection[1])
+print("QuickSort time: \t\t%s" % quick[1])
+print("MergeSort time: \t\t%s" % merge[1])
